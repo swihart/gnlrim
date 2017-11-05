4a7bf942dbfc018b7fa28918e37e28492df56b5c
swihart <bruce.swihart@gmail.com>
2017-11-05 18:26:55 -0500

    scratch_testing libstableR alpha=2 constant

--- scratch_testing.R
+++ scratch_testing.R
@@ -67,6 +67,172 @@ gnlrim_fit2$se
 ##############################################################
 ## this section tests the libstableR implementation of
 ## subgauss and compares it to stabledist implementation of
+## subgauss and compares to PPN with [dp]norm
+##############################################################
+##############################################################
+##############################################################
+
+dose <- c(c(9,12,4,9,11,10,2,11,12,9,9,9,4,9,11,9,14,7,9,8), 3+c(9,12,4,9,11,10,2,11,12,9,9,9,4,9,11,9,14,7,9,8))
+y_01 <- rep(as.numeric(c(8.674419, 11.506066, 11.386742, 27.414532, 12.135699,  4.359469,
+                         1.900681, 17.425948,  4.503345,  2.691792,  5.731100, 10.534971,
+                         11.220260,  6.968932,  4.094357, 16.393806, 14.656584,  8.786133,
+                         20.972267, 17.178012) > 4),2)
+id <- rep(1:8, each=5)
+
+y_cbind = cbind(y_01, 1-y_01)
+
+fit_PPN_scl <- gnlrim(y=y_cbind,
+                      mu=~pnorm(a+b*dose+rand),
+                      pmu=c(a=0,b=0),
+                      pmix=c(scl=1),
+                      p_uppb = c(Inf ,  Inf, Inf),
+                      p_lowb = c(-Inf, -Inf,   0+1e-5),
+                      distribution="binomial",
+                      nest=id,
+                      random="rand",
+                      mixture="normal-var")
+fit_PPN_scl$coefficients
+fit_PPN_scl$se
+
+
+#Now try reproducing PPN with *!*stabledist*!* subgauss:
+
+fit_PPN_stabledist_scl <- gnlrim(y=y_cbind,
+                                 mu=~pstable(a+b*dose+rand, alpha, 0, 1/sqrt(2), 0),
+                                 pmu=c(a=0,b=0, alpha=2),
+                                 pmix=c(alpha=2, scl=1),
+                                 p_uppb = c(Inf ,  Inf, 2, Inf),
+                                 p_lowb = c(-Inf, -Inf, 2,   0+1e-5),
+                                 distribution="binomial",
+                                 nest=id,
+                                 random="rand",
+                                 mixture="stabledist-subgauss-scl",
+                                 ooo=TRUE,
+                                 trace=1)
+fit_PPN_stabledist_scl$coefficients
+fit_PPN_stabledist_scl$se
+
+#Now try reproducing PPN with **libstableR** subgauss:
+
+fit_PPN_libstableR_scl <- gnlrim(y=y_cbind,
+                                 mu=~ stable_cdf(a+b*dose+rand, c(alpha, 0, 1/sqrt(2), 0)),
+                                 pmu=c(a=0,b=0, alpha=2),
+                                 pmix=c(alpha=2, scl=1),
+                                 p_uppb = c(Inf ,  Inf, 2, Inf),
+                                 p_lowb = c(-Inf, -Inf, 2,   0+1e-5),
+                                 distribution="binomial",
+                                 nest=id,
+                                 random="rand",
+                                 mixture="libstableR-subgauss-scl",
+                                 ooo=TRUE,
+                                 trace=1
+                                 )
+
+## should be 0s, not NAs.
+attr(fit_PPN_libstableR_scl, "details")[1,]$nhatend
+bonk <- attr(fit_PPN_libstableR_scl, "details")[1,]$nhatend
+bonk_no_na <- matrix(bonk[!is.na(bonk)], nrow=3)
+bonk_no_na_cov <- solve(bonk_no_na)
+bonk_se <- sqrt(diag(bonk_no_na_cov))
+## compared these two:
+bonk_se
+fit_PPN_scl$se
+
+
+
+fit_PPN_libstableR_scl$coefficients
+fit_PPN_libstableR_scl$se
+
+## what if we made a wrapper for libstableR
+## that would take out of bound pars and force them in bounds
+## ***internally***
+stable_cdf2 <- function(x, pars, parameterization=0L, tol=1e-12){
+  libstableR::stable_cdf(x,
+                         c(min(max(pars[1],0+1e-20),2),
+                           min(max(pars[2],-1),1),
+                           min(max(pars[3],0+1e-20),Inf),
+                           pars[4]),
+                         parameterization,
+                         tol
+  )
+}
+
+stable_pdf2 <- function(x, pars, parameterization=0L, tol=1e-12){
+  libstableR::stable_pdf(x,
+                         c(min(max(pars[1],0+1e-20),2),
+                           min(max(pars[2],-1),1),
+                           min(max(pars[3],0+1e-20),Inf),
+                           pars[4]),
+                         parameterization,
+                         tol
+  )
+}
+
+
+## alpha <= 2 is forced internally
+stable_cdf2(1, c( 2.0,0,1,0))
+stable_cdf2(1, c( 2.1,0,1,0))
+stable_cdf2(1, c(22.1,0,1,0))
+
+## alpha >= 0+1e-20 is forced internally
+stable_cdf2(1, c( 0+1e-20,0,1,0))
+stable_cdf2(1, c( 0,0,1,0))
+stable_cdf2(1, c(22.1,0,1,0))
+
+## beta >= -1 is forced internally
+stable_cdf2(1, c( 1,-1.0,1,0))
+stable_cdf2(1, c( 1,-1.1,1,0))
+stable_cdf2(1, c( 1,-2,1,0))
+
+
+fit2_PPN_libstableR_scl <- gnlrim(y=y_cbind,
+                                 mu=~ stable_cdf2(a+b*dose+rand, c(alpha, 0, 1/sqrt(2), 0)),
+                                 pmu=c(a=0,b=0, alpha=2),
+                                 pmix=c(alpha=2, scl=1),
+                                 p_uppb = c(Inf ,  Inf, 2, Inf),
+                                 p_lowb = c(-Inf, -Inf, 2,   0+1e-5),
+                                 distribution="binomial",
+                                 nest=id,
+                                 random="rand",
+                                 mixture="libstableR-subgauss-scl",
+                                 ooo=TRUE,
+                                 trace=1
+)
+
+## should be 0s, not NAs.
+attr(fit2_PPN_libstableR_scl, "details")[1,]$nhatend
+bonk <- attr(fit2_PPN_libstableR_scl, "details")[1,]$nhatend
+bonk_no_na <- matrix(bonk[!is.na(bonk)], nrow=3)
+bonk_no_na_cov <- solve(bonk_no_na)
+bonk_se <- sqrt(diag(bonk_no_na_cov))
+## compared these two:
+bonk_se
+fit_PPN_scl$se
+
+
+
+fit2_PPN_libstableR_scl$coefficients
+fit2_PPN_libstableR_scl$se
+
+
+
+
+
+## all the same?
+fit_PPN_scl$coefficients
+fit_PPN_stabledist_scl$coefficients
+fit_PPN_libstableR_scl$coefficients
+
+fit_PPN_scl$se
+fit_PPN_stabledist_scl$se
+fit_PPN_libstableR_scl$se
+
+
+##############################################################
+##############################################################
+##############################################################
+## this section tests the libstableR implementation of
+## subgauss and compares it to stabledist implementation of
 ## subgauss and compares to CCC with [dp]cauchy
 ##############################################################
 ##############################################################
